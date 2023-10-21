af0e45e19b5be4e3f7d7342a6450cecd4410bbd1
SuHwanYoon <ehtldy@naver.com>
2023-10-21 21:47:00 +0900

    add module Components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { Component } from 'react';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent/>
-      <SecondComponent/>
-      <ThirdComponent/>
-      <FourthComponent/>
+      <LearningComponent></LearningComponent>
     </div>
   );
-}
-
-function FirstComponent(){
-  return(
-    <div className='FirstComponent'>First Component</div>
-  )
-}
-
-
-function SecondComponent(){
-  return(
-    <div className='SecondComponent'>Second Component</div>
-  )
-}
-
-class ThirdComponent extends Component{
-  render(){
-    return(
-      <div className='ThirdComponent'>Third Component</div>
-    )
-  }
-}
-
-class FourthComponent extends Component{
-  render(){
-    return(
-      <div>
-        <div className='FourthComponent'>Fourth Component</div>
-        <div className='FourthComponent'>Fourth Component</div>
-      </div>
-    )
-  }
 }
 
 export default App;
