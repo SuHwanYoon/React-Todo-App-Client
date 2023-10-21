b97b1702135ac7fe2ecb36cd3f984be5c0033554
SuHwanYoon <ehtldy@naver.com>
2023-10-21 16:40:25 +0900

    add Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application Updated
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <ForthComponent></ForthComponent>
     </div>
   );
+}
+
+function FirstComponent(){
+  return(
+    <div className='FirstComponent'>First Component</div>
+  )
+}
+
+
+function SecondComponent(){
+  return(
+    <div className='SecondComponent'>Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component{
+  render(){
+    return(
+      <div className='ThirdComponent'>Third Component</div>
+    )
+  }
+}
+
+class ForthComponent extends Component{
+  render(){
+    return(
+      <div className='ForthComponent'>Forth Component</div>
+    )
+  }
 }
 
 export default App;
