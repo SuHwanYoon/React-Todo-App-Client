8df352b0deb0cc05df12a6df02bce55b5813b03e
SuHwanYoon <ehtldy@naver.com>
2024-06-27 17:53:55 +0900

    add form validation logic

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { getSpecificTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
+import { ErrorMessage, Field, Form, Formik } from "formik"
 
 export default function TodoComponent() {
     //parameter값 - id 를 사용하기 위해 useParams Hook사용
@@ -10,8 +11,10 @@ export default function TodoComponent() {
     const authContext = useAuth()
     //인증 컨텍스트에서 인증된 username가져오기
     const username = authContext.username
-    //
+    //할일 내용 설정하기
     const [description,setDescription] = useState('')
+    //날짜 설정하기
+    const [targetDate,setTargetDate] = useState('')
 
     //TodoComponet가 라우팅 될때마다 getSpecificTodo()를 실행
     //의존성배열에 포함된 [id]값이 변경될때도 getSpecificTodo()를 또다시 호출
@@ -23,16 +26,40 @@ export default function TodoComponent() {
         }, [id]
     )
 
-    //특정 Todo 호출 api 함수
+    //특정 Todo 호출 api 함수 (update 버튼 클릭시)
     function getSpecificTodo() {
         getSpecificTodoApi(username,id)
         .then(response => {
-            // 호출성공시 description값을 가져와서 set해준다
+            console.log(response)
+            // 호출성공시 api의 description값을 가져와서 set해준다
             setDescription(response.data.description)
+            // 호출성공시 api의 targetDate값을 가져와서 set해준다
+            setTargetDate(response.data.targetDate)
         }
     )
         .catch(error => console.log(error))
     }
+
+    function onSubmit(values) {
+        console.log(values)
+    }
+    //formik 유효성 검사 함수
+    function validate(values) {
+        //반환할 에러 객체
+        let errors =  {
+            // description: "Enter a vaild description" ,
+            // targetDate: "Enter a valid targetDate"
+        }
+        if (values.description.length < 5) {
+            errors.description = 'Enter at least 5 characters'
+        }
+        if (values.targetDate === null) {
+            errors.targetDate = 'Enter a targetDate'
+        }
+        console.log(values)
+        return errors
+    }
+
     return (
 
 
@@ -40,7 +67,55 @@ export default function TodoComponent() {
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                description: {description}
+                {/* api에서 가져온 값을 폼의 입력 초기값으로 설정 name="" 에 매핑 */}
+                {/* initialValues가 변경될 때마다 폼의 입력값이 새로운 값으로 다시 초기화*/}
+                {/* Save 버튼으로 제출시 onSubmit 메서드 실행 */}
+                {/* 내부적으로 필드값이 submit 되기전에 유효성검사를 실행 */}
+                {/* 폼 필드 입력이 변할때마다 유효성검사하는것을 비활성화 */}
+                {/* 폼 필드에 포커스가 벗어났을때 유효성검사하는것을 비활성화 */}
+                <Formik initialValues={ {description, targetDate} }
+                 enableReinitialize={true}
+                 onSubmit={onSubmit}
+                 validate={validate}
+                 validateOnChange = {false}
+                 validateOnBlur = {false}
+                 >
+                {/* JSX 리턴함수 설정 */}
+                    {
+                        (props) => (
+                            <Form>
+                                {/* description 에러메세지 표시 */}
+                                <ErrorMessage 
+                                    name="description"
+                                    // 에러메세지를 div로서 표시
+                                    component="div"
+                                    className="alert alert-warning"
+                                />
+                                {/* targetDate 에러메세지 표시 */}
+                                <ErrorMessage 
+                                    name="targetDate"
+                                    // 에러메세지를 div로서 표시
+                                    component="div"
+                                    className="alert alert-warning"
+                                />
+                                {/* bootstrap form-group , form-control */}
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset>
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field type="date" className="form-control" name="targetDate"/>
+                                </fieldset>
+                                <div>
+                                    {/* formik를 사용해서 input태그에 onChange를 쓰지않아도 자동으로 변화된 입력데이터를 제출 */}
+                                    <button className="btn btn-success m-5" type="submit">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+
+                </Formik>
             </div>
         </div>
     )
