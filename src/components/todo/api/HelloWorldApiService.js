14e5253ce26b63e98b7a907b8a8b9d456fba33a7
SuHwanYoon <ehtldy@naver.com>
2024-07-01 22:54:29 +0900

    add token Auth

--- src/components/todo/api/HelloWorldApiService.js
+++ src/components/todo/api/HelloWorldApiService.js
@@ -6,27 +6,44 @@ import axios from "axios";
 
 //axios 인스턴스 생성
 const apiClient = axios.create(
-    //인스턴스에 기본 url객체만들기
-    {
-        baseURL: 'http://localhost:8080'
-    }
-)
-
+  //인스턴스에 기본 url객체만들기
+  {
+    baseURL: "http://localhost:8080",
+  }
+);
 
 //화살표 함수로 표현
-export const getHelloWorldBean 
-    = () =>apiClient.get("/hello-world-bean");
+export const getHelloWorldBean = () => apiClient.get("/hello-world-bean");
 
-// Response to preflight request doesn't pass access control check
-export const getHelloWorldBeanPathVariable 
-        //템플릿 리터럴을 사용해서 paramater의 변수값을 ${username}을 통해 삽입
-    = (username) => apiClient.get(`/hello-world/path-variable/${username}`,
-        //SpringSecurity를 설정했기 때문에 인증객체 건네기
-        {
-            //헤더객체
-            headers: {
-                //SpringScutiry에서 설정된 name,PW의 인코딩 문자열 
-                Authorization: 'Basic eW9vbjpkdW1teQ=='
-            }
+//  preflight Options메서드 리퀘스트 요청은 서버쪽에서 설정해준다
+export const getHelloWorldBeanPathVariable =
+  //템플릿 리터럴을 사용해서 paramater의 변수값을 ${username}을 통해 삽입
+  (username) =>
+    apiClient.get(
+      `/hello-world/path-variable/${username}`,
+      //SpringSecurity를 설정했기 때문에 인증객체 건네기
+      {
+        //헤더객체
+        headers: {
+          //SpringScutiry에서 설정된 username,password의 인코딩 문자열(token)
+          Authorization: "Basic eW9vbjpkdW1teQ==",
+        },
+      }
+    );
+
+//  preflight Options메서드 리퀘스트 요청은 서버쪽에서 설정해준다
+//기본인증 URL 호출 service
+export const getBasicAuthCheck =
+  //클라이언트에서 token을  paramater로 서버로 보내기
+  (token) =>
+    apiClient.get(
+      `/basicauth`,
+      //SpringSecurity를 설정했기 때문에 인증객체 건네기
+      {
+        //헤더객체
+        headers: {
+          //Authorization 프로퍼티에 token을 담는다 
+          Authorization: token
         }
+      }
     );
--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { getBasicAuthCheck} from "../api/HelloWorldApiService";
 
 //다른 컴포넌트에 인증상태를 공유할 컨텍스트 생성
 export const AuthContext = createContext();
@@ -21,22 +22,51 @@ export default function AuthProvider({ children }) {
   const [username, setUsername] = useState(null);
   
 
-  //로그인 인증 관련 함수
-  function login(username, password) {
-    if (username === 'yoon' && password === 'dummy') {
-        //로그인 성공시 setAuthenticated 메서드에 true값 설정
-        setAuthenticated(true)
-        //로그인 성공시 화면에서 받은 username 값 설정
-        setUsername(username)
-        return true
-    }else{
-        //로그인 실패시 setAuthenticated메서드에 false값 설정
-        setAuthenticated(false)
-        //로그인 실패시 null값 설정
-        setUsername(null)
-        return false
+  //로그인 인증 관련 함수(하드코딩)
+  // function login(username, password) {
+  //   if (username === 'yoon' && password === 'dummy') {
+  //       //로그인 성공시 setAuthenticated 메서드에 true값 설정
+  //       setAuthenticated(true)
+  //       //로그인 성공시 화면에서 받은 username 값 설정
+  //       setUsername(username)
+  //       return true
+  //   }else{
+  //       //로그인 실패시 setAuthenticated메서드에 false값 설정
+  //       setAuthenticated(false)
+  //       //로그인 실패시 null값 설정
+  //       setUsername(null)
+  //       return false
+  //   }
+  // }
+  
+    //로그인 인증 관련 함수(기본인증)
+    function login(username, password) {
+
+      //기본인증토큰 표준로직 -> 'Basic(한칸공백)' + (username + ':' + password)토큰
+      //Base64 인코딩 - window.btoa(ASCII 문자열)
+      //SpringSecurity설정에 username = yoon password= dummy 설정이 되있음
+      const basicToken = 'Basic ' + window.btoa(username + ':' + password)
+      //인코딩된 token으로  기본인증 URL 호출 
+      getBasicAuthCheck(basicToken)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+
+      setAuthenticated(false)
+      // if (username === 'yoon' && password === 'dummy') {
+      //     //로그인 성공시 setAuthenticated 메서드에 true값 설정
+      //     setAuthenticated(true)
+      //     //로그인 성공시 화면에서 받은 username 값 설정
+      //     setUsername(username)
+      //     return true
+      // }else{
+      //     //로그인 실패시 setAuthenticated메서드에 false값 설정
+      //     setAuthenticated(false)
+      //     //로그인 실패시 null값 설정
+      //     setUsername(null)
+      //     return false
+      // }
     }
-  }
+
   //로그아웃 함수
     function logout() {
         setAuthenticated(false)
