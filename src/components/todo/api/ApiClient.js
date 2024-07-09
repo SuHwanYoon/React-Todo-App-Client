a5b5deff395aab24f4581a4c9894585d14661793
SuHwanYoon <ehtldy@naver.com>
2024-07-09 19:44:57 +0900

    add baseURL to Connect ElasticBeanStalk RestAPI

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -5,6 +5,8 @@ import axios from "axios";
 export const apiClient = axios.create(
   //인스턴스에 기본 url객체만들기
   {
-    baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
+    //Elastic BeanStalk 배포 RestApi에 접속하기 위한 로컬 기본URL
+    baseURL: "http://localhost:5000",
   }
 );
