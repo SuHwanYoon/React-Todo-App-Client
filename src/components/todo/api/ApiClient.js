de2027805220eaf3724675fc5ec98ff5e545bd43
SuHwanYoon <ehtldy@naver.com>
2024-07-10 19:05:32 +0900

    branch test2

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -6,7 +6,9 @@ export const apiClient = axios.create(
   //인스턴스에 기본 url객체만들기
   {
     // baseURL: "http://localhost:8080",
-    //Elastic BeanStalk 배포 RestApi에 접속하기 위한 로컬 기본URL
-    baseURL: "http://localhost:5000",
+    //Elastic BeanStalk 권장 port 로컬 기본URL server.port=5000
+    // baseURL: "http://localhost:5000",
+    // Elastic BeanStalk rest-api URL
+    baseURL: "http://full-stack-restapi-h2-env.eba-brpvjp4b.ap-northeast-2.elasticbeanstalk.com/",
   }
 );
