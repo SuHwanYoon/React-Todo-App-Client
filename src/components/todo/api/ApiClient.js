5d2d4ebd9b1617a0d176ec20f790b81ab7a78bc7
SuHwanYoon <ehtldy@naver.com>
2024-07-12 02:31:55 +0900

    add cors anywhere

--- src/components/todo/api/ApiClient.js
+++ src/components/todo/api/ApiClient.js
@@ -9,6 +9,7 @@ export const apiClient = axios.create(
     //Elastic BeanStalk 권장 port 로컬 기본URL server.port=5000
     // baseURL: "http://localhost:5000",
     // Elastic BeanStalk rest-api URL
-    baseURL: "http://full-stack-restapi-mysql-env.eba-thy63jtv.ap-northeast-2.elasticbeanstalk.com/",
+    // baseURL: "http://full-stack-restapi-mysql-env.eba-thy63jtv.ap-northeast-2.elasticbeanstalk.com/",
+    baseURL: "https://cors-anywhere.herokuapp.com/http://full-stack-restapi-mysql-env.eba-thy63jtv.ap-northeast-2.elasticbeanstalk.com",
   }
 );
